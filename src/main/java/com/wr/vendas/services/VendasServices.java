379531bf489c7929910c41c787f1c010822ba26f
Wesly <weslyramalho@yahoo.com.br>
2022-02-09 19:21:14 -0300

    Implementado metodos na class vendas services

--- src/main/java/com/wr/vendas/services/VendasServices.java
+++ src/main/java/com/wr/vendas/services/VendasServices.java
@@ -1,5 +1,22 @@
 package com.wr.vendas.services;
 
-public class VendasServices {
+import java.util.List;
+
+import com.wr.vendas.entities.Produtos;
+
+public class VendasServices implements IVendasServices{
+
+	@Override
+	public List<Produtos> venderProduto(Double total) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Double desconto(Integer desconto) {
+		
+		return null;
+	}
+	
 
 }
