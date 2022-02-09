291a836f6c5b3ba52d2a54479767e7c2baecbe79
Wesly <weslyramalho@yahoo.com.br>
2022-02-09 15:14:27 -0300

    Criado class produtos

---
+++ src/main/java/com/wr/vendas/entities/Produtos.java
@@ -0,0 +1,51 @@
+package com.wr.vendas.entities;
+
+public class Produtos {
+	private Long id;
+	private String nome;
+	private Long quantidadeEstoque;
+	private Double preco;
+	private Double maxDescontoPermitido;
+	
+	public Produtos() {
+		
+	}
+	public Produtos(Long id, String nome, Long quantidadeEstoque, Double preco, Double maxDescontoPermitido) {
+		this.id = id;
+		this.nome = nome;
+		this.quantidadeEstoque = quantidadeEstoque;
+		this.preco = preco;
+		this.maxDescontoPermitido = maxDescontoPermitido;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Long getQuantidadeEstoque() {
+		return quantidadeEstoque;
+	}
+	public void setQuantidadeEstoque(Long quantidadeEstoque) {
+		this.quantidadeEstoque = quantidadeEstoque;
+	}
+	public Double getPreco() {
+		return preco;
+	}
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	public Double getMaxDescontoPermitido() {
+		return maxDescontoPermitido;
+	}
+	public void setMaxDescontoPermitido(Double maxDescontoPermitido) {
+		this.maxDescontoPermitido = maxDescontoPermitido;
+	}
+
+}
