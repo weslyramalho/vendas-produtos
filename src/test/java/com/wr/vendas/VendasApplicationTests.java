9aebbe2433c21abe85fbfa2bf9066c324f1a3be0
Wesly <weslyramalho@yahoo.com.br>
2022-02-11 17:43:55 -0300

    impelemntyando teste

--- src/test/java/com/wr/vendas/VendasApplicationTests.java
+++ src/test/java/com/wr/vendas/VendasApplicationTests.java
@@ -1,13 +1,35 @@
 package com.wr.vendas;
 
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.wr.vendas.entities.Produtos;
 
 @SpringBootTest
 class VendasApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void calculandoValorTotalDoprecoDoProdutoComDesconto() {
+		Produtos produtos = new Produtos(1l,"carro", 10l, 100.0, 0.10);
+		Double resultado = produtos.getTamanhoDesconto(0.10);
+		assertEquals(90.0, resultado);
+	
+	}
+	
+	@Test
+	void calculandoValorTotalDoprecoDoProdutoComDescontoAcimadoDoValorpermitido() {
+		Produtos produtos = new Produtos(1l,"carro", 10l, 100.0, 0.10);
+		Double resultado = produtos.getTamanhoDesconto(0.15);
+		assertEquals(90.0, resultado);
+	
+	}
+	@Test
+	void calculandoValorTotalDoprecoDoProdutoComDescontoAbaixoDoValorpermitido() {
+		Produtos produtos = new Produtos(1l,"carro", 10l, 100.0, 0.10);
+		Double resultado = produtos.getTamanhoDesconto(0.05);
+		assertEquals(95.0, resultado);
+	
 	}
 
 }
