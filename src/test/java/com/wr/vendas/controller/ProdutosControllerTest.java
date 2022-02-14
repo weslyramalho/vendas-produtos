e83b82e066dc29eec3b4a5d8a34d771cd1850231
Wesly <weslyramalho@yahoo.com.br>
2022-02-14 16:26:31 -0300

    criando testes

---
+++ src/test/java/com/wr/vendas/controller/ProdutosControllerTest.java
@@ -0,0 +1,22 @@
+package com.wr.vendas.controller;
+
+public class ProdutosControllerTest {
+	
+	private static final String MOCKED_RESULT = "{\"produtos\" :[\n"+
+			" {\n" +
+			" \"id\": 1, \n" +
+			" \"nome\": \"TESTE\", \n" +
+			" \"quantidadeEstoque\": 20, \n" +
+			" \"preco\": 50.0, \n" +
+			" \"maxDescontoPermitido\": 0.5, \n" +
+			" }, \n" +
+			" {\n" +
+			" \"id\": 2, \n" +
+			" \"nome\": \"TESTE2\", \n" +
+			" \"quantidadeEstoque\": 40, \n" +
+			" \"preco\": 100.0, \n" +
+			" \"maxDescontoPermitido\": 0.5, \n" +
+			" }, \n" +
+			"]}";
+
+}
