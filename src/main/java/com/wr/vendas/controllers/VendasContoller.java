bf0ac470a1b7e01f81afb200639755cb7faf5395
Wesly <weslyramalho@yahoo.com.br>
2022-02-16 14:12:30 -0300

    Importado VendasRepository e VendasServices em VendasController

--- src/main/java/com/wr/vendas/controllers/VendasContoller.java
+++ src/main/java/com/wr/vendas/controllers/VendasContoller.java
@@ -1,5 +1,20 @@
 package com.wr.vendas.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wr.vendas.repositories.VendasRepository;
+import com.wr.vendas.services.VendasServices;
+
+@RestController
+@RequestMapping("/vendas")
 public class VendasContoller {
+	
+	@Autowired
+	VendasServices vendasServ;
+	
+	@Autowired
+	VendasRepository vendasRep;
 
 }
