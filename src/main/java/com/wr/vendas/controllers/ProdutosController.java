0a9c1c107d40e5b1548e177d01334f802f6222c6
Wesly <weslyramalho@yahoo.com.br>
2022-02-16 14:07:20 -0300

    update implementado em produtosContoller

--- src/main/java/com/wr/vendas/controllers/ProdutosController.java
+++ src/main/java/com/wr/vendas/controllers/ProdutosController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -44,5 +45,13 @@ public class ProdutosController {
 	        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 
 	  }
+	
+
+	  @PatchMapping
+	  public ResponseEntity<Object> update(@RequestBody final Produtos produtos){
+	        prodServ.update(produtos);
+	        final String up = "Produto atualizado";
+	        return new ResponseEntity<>(up,HttpStatus.NO_CONTENT);
+	    }
 
 }
